218831bb3c692fae98c846f6e58d8395dc06fc6f
crimaniak <crimaniak@gmail.com>
2018-05-08 20:02:13 +0300

    example update

--- examples/ifupdated/compileme.sh
+++ examples/ifupdated/compileme.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 FLAGS="-O -release -noboundscheck -I=$SCRIPT_DIR/../../src"
 ifupdated dmd -c $FLAGS ../../src/app.d
 ifupdated dmd -c $FLAGS ../../src/ifupdated/runner.d
 ifupdated dmd -c $FLAGS ../../src/ifupdated/db.d
-ifupdated dmd -of=ifup $FLAGS app.o runner.o db.o
+ifupdated dmd -of=ifupdated $FLAGS app.o runner.o db.o
